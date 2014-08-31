6e069c4605ce169374f0e757025275f565498ddd
Ryan Popa <ryanp@recsolu.com>
2014-08-31 14:04:10 -0500

    Adding settings view controller

---
+++ ApTracker/RPSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPSettingsViewController.h
+//  ApTracker
+//
+//  Created by Ryan Popa on 8/31/14.
+//  Copyright (c) 2014 Ryan Popa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPSettingsViewController : UIViewController
+
+@end
