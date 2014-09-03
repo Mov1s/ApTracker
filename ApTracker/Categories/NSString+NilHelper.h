361bac53bcd870d869566496762ce84cfe33dc85
Ryan Popa <ryanp@recsolu.com>
2014-09-02 19:57:56 -0500

    Moving the settings screen first Validating settings before save Making tracking manager pull from saved settings

---
+++ ApTracker/Categories/NSString+NilHelper.h
@@ -0,0 +1,15 @@
+//
+//  NSString+isNilOrEmpty.h
+//  Collect
+//
+//  Created by Ryan Popa on 7/8/13.
+//  Copyright (c) 2013 Ryan Popa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (NilHelper)
+
++ (BOOL)stringIsNilOrEmpty: (NSString *)aString;
+
+@end
--- ApTracker/RPSettingsViewController.m
+++ ApTracker/RPSettingsViewController.m
@@ -6,7 +6,9 @@
 //  Copyright (c) 2014 Ryan Popa. All rights reserved.
 //
 
+#import "NSString+NilHelper.h"
 #import "RPSettingsViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 //Keys for NSUserDefaults dictionary
 NSString * const kRPSettingsNickKey = @"kRPSettingsNickKey";
@@ -18,27 +20,32 @@ NSString * const kRPSettingsPortKey = @"kRPSettingsPortKey";
 @property (weak, nonatomic) IBOutlet UITextField *nickTextField;
 @property (weak, nonatomic) IBOutlet UITextField *hostTextField;
 @property (weak, nonatomic) IBOutlet UITextField *portTextField;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
 @end
 
 @implementation RPSettingsViewController
 
 - (void)viewDidLoad
 {
+    //Style the save button
+    self.saveButton.titleLabel.font = [UIFont fontWithName: @"Open Sans Regular" size: 42];
+    self.saveButton.layer.borderColor = [[UIColor yellowColor] CGColor];
+    self.saveButton.layer.borderWidth = 2.0;
+    self.saveButton.layer.cornerRadius = 7.0;
+    
     //Restore any saved settings to the text fields
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     self.nickTextField.text = [defaults objectForKey: kRPSettingsNickKey];
     self.hostTextField.text = [defaults objectForKey: kRPSettingsHostKey];
     self.portTextField.text = [defaults objectForKey: kRPSettingsPortKey];
-}
-
-- (void)viewWillDisappear: (BOOL)animated
-{
-    //Save settings
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject: self.nickTextField.text forKey: kRPSettingsNickKey];
-    [defaults setObject: self.hostTextField.text forKey: kRPSettingsHostKey];
-    [defaults setObject: self.portTextField.text forKey: kRPSettingsPortKey];
-    [defaults synchronize];
+    
+    //Continue straight to the stats if settings have already been configured
+    if ([self settingsBoxesHaveValues])
+    {
+        [self performSegueWithIdentifier: @"statsSegue" sender: self];
+        [self.saveButton setTitle: @"Save" forState: UIControlStateNormal];
+        return;
+    }
 }
 
 #pragma mark - TextField Delegate
@@ -59,6 +66,47 @@ NSString * const kRPSettingsPortKey = @"kRPSettingsPortKey";
         [self.portTextField resignFirstResponder];
     
     return NO;
+}
+
+#pragma mark - Actions
+//Action for the save button
+//Validates the settings and continues to the stats
+- (IBAction)saveAction: (UIButton *)sender
+{
+    //Continue if things are valid
+    if ([self settingsBoxesHaveValues])
+    {
+        //Save settings
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        [defaults setObject: self.nickTextField.text forKey: kRPSettingsNickKey];
+        [defaults setObject: self.hostTextField.text forKey: kRPSettingsHostKey];
+        [defaults setObject: self.portTextField.text forKey: kRPSettingsPortKey];
+        [defaults synchronize];
+
+        //Move to the stats screen
+        [self performSegueWithIdentifier: @"statsSegue" sender: self];
+    }
+    
+    //Show message otherwise
+    else
+        [self displayAlert: @"All settings are required."];
+}
+
+#pragma mark - Helpers
+//Checks that all boxes have a value
+- (BOOL)settingsBoxesHaveValues
+{
+    BOOL hostValue = ![NSString stringIsNilOrEmpty: self.hostTextField.text];
+    BOOL portValue = ![NSString stringIsNilOrEmpty: self.portTextField.text];
+    BOOL nickValue = ![NSString stringIsNilOrEmpty: self.nickTextField.text];
+    return hostValue && portValue && nickValue;
+}
+
+//Shows a message
+- (void)displayAlert: (NSString *)message
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: nil message: message delegate: nil cancelButtonTitle: @"Ok" otherButtonTitles: nil];
+    [alert show];
 }
 
 @end
